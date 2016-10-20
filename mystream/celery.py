b17c59041d974913791d361e77f2e845d151878e
Maël Regnery <mregnery@waycom.net>
2016-10-20 13:28:07 +0200

    Delete djcelery trace

--- mystream/celery.py
+++ mystream/celery.py
@@ -9,16 +9,3 @@ app = Celery('mystream')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-#app.conf.update(
-#    CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
-#)
-#
-#app.conf.update(
-#    CELERY_RESULT_BACKEND='djcelery.backends.cache:CacheBackend',
-#)
-
-#@app.task(bind=True)
-#def debug_task(self):
-#    print('Request: {0!r}'.format(self.request))
-#
--- mystream/settings.py
+++ mystream/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library',
-    'djcelery',
     'kombu.transport.django',
 ]
 
